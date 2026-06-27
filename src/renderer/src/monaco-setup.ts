fa6a6481f6f2aaaa627665af961e6704ec6e1c78
Angelo Pereira <angelojrp@gmail.com>
2026-06-27 17:32:36 -0300

    feat(editor): autocomplete SQL ciente do schema
    - Provedor de completion no Monaco com tabelas/views e palavras-chave
    - 'tabela.' sugere colunas (lazy-load + cache) da conexão da aba
    - App alimenta o schema da conexão ativa; cache por conexão
    
    Closes #7

--- src/renderer/src/monaco-setup.ts
+++ src/renderer/src/monaco-setup.ts
@@ -1,6 +1,7 @@
 import * as monaco from 'monaco-editor'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import { loader } from '@monaco-editor/react'
+import { registerSqlCompletion } from './sqlCompletion'
 
 // Carrega o Monaco localmente (sem CDN) e configura o worker do editor.
 self.MonacoEnvironment = {
@@ -9,4 +10,5 @@ self.MonacoEnvironment = {
   }
 }
 
+registerSqlCompletion(monaco)
 loader.config({ monaco })
